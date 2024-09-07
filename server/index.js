a0ef290d071ede586803801fe1bf68ae4898028c
Nilesh Sargara <121281018+nilesh2604s@users.noreply.github.com>
2024-09-07 21:33:53 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,8 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.use(cors({
-  origin: 'https://nilesh-chat.netlify.app/'
+app.use(cors({ '*'
 }));
 app.use(router);
 
